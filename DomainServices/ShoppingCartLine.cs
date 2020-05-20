000c6337e5094d0e704a33c349018ba55624991a
Matías Araya <matiasaraya28@gmail.com>
2020-05-20 00:53:45 -0300

    Add new Service class

---
+++ DomainServices/ShoppingCartLine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainServices
+{
+    public class ShoppingCartLine
+    {
+        public string CodeProduct { get; private set; }
+        public int Amount { get; private set; }
+        public decimal Subtotal { get; private set; }
+    }
+}
